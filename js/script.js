523d705959fdfd6baf2e3e62dd00ebe0545fca2f
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-10-05 19:29:44 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let question = prompt(`Выберите операцию: add, sub, mult, div`);
+let numOne = +prompt(`Введите первое число:`);
+let numTwo = +prompt(`Введите второе число:`);
+if (question  === `add`) {
+   alert( `${numOne} + ${numTwo} = ${numOne + numTwo}`);
+} else if  (question  === `sub`) {
+   alert( `${numOne} - ${numTwo} = ${numOne - numTwo}`);
+} else if (question  === `mult`) {
+   alert( `${numOne} * ${numTwo} = ${numOne * numTwo}`);
+} else if (question  === `div`) {
+    alert(`${numOne} / ${numTwo} = ${numOne / numTwo}`);
+} else {
+    alert(`Вероятно вы ввели что-то неправильно`)
+}
